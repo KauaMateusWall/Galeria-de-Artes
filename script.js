114cc6599b9eb48adb8700460d5114b8000d0263
KauaMateusWall <151979897+KauaMateusWall@users.noreply.github.com>
2025-05-12 17:41:48 -0300

    arrumado

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('#ArtistasAbrir').click(function() {
+        $('#Artistas').toggle();
+    });
+
+    $('.toggle-btn').click(function() {
+        var targetId = $(this).attr('id').replace('Abrir', '');
+        $('#' + targetId).toggle();
+    });
+});
+
+function validarFormulario() {
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let mensagem = document.getElementById('mensagem').value;
+
+    if (!nome || !email || !mensagem) {
+        alert('Todos os campos são obrigatórios.');
+        return false;
+    }
+
+    alert('Mensagem enviada com sucesso!');
+    return true;
+}
