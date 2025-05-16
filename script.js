aaba65a2994d673c5d3c6e9418e49445fb43b38c
Kaua Mateus Wall <151979897+KauaMateusWall@users.noreply.github.com>
2025-05-16 20:12:07 -0300

    formulario

--- script.js
+++ script.js
@@ -1,24 +1,61 @@
 $(document).ready(function() {
     $('#ArtistasAbrir').click(function() {
-        $('#Artistas').slideToggle(300); // 300ms de animação
+        $('#Artistas').slideToggle(300); 
     });
 
     $('.toggle-btn').click(function() {
         var targetId = $(this).attr('id').replace('Abrir', '');
-        $('#' + targetId).slideToggle(300); // mesma animação suave
+        $('#' + targetId).slideToggle(300); 
     });
 });
 
-function validarFormulario() {
-    let nome = document.getElementById('nome').value;
-    let email = document.getElementById('email').value;
-    let mensagem = document.getElementById('mensagem').value;
-
-    if (!nome || !email || !mensagem) {
-        alert('Todos os campos são obrigatórios.');
-        return false;
+function limpa_formulário_cep() {
+            document.getElementById('rua').value=("");
+            document.getElementById('bairro').value=("");
+            document.getElementById('cidade').value=("");
+            document.getElementById('uf').value=("");
+            document.getElementById('ibge').value=("");
     }
 
-    alert('Mensagem enviada com sucesso!');
-    return true;
-}
+    function meu_callback(conteudo) {
+        if (!("erro" in conteudo)) {
+            document.getElementById('rua').value=(conteudo.logradouro);
+            document.getElementById('bairro').value=(conteudo.bairro);
+            document.getElementById('cidade').value=(conteudo.localidade);
+            document.getElementById('uf').value=(conteudo.uf);
+            document.getElementById('ibge').value=(conteudo.ibge);
+        } 
+        else {
+            limpa_formulário_cep();
+            alert("CEP não encontrado.");
+        }
+    }
+        
+    function pesquisacep(valor) {
+
+        var cep = valor.replace(/\D/g, '');
+
+        if (cep != "") {
+            var validacep = /^[0-9]{8}$/;
+            if(validacep.test(cep)) {
+
+                document.getElementById('rua').value="...";
+                document.getElementById('bairro').value="...";
+                document.getElementById('cidade').value="...";
+                document.getElementById('uf').value="...";
+                document.getElementById('ibge').value="...";
+
+                var script = document.createElement('script');
+                script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+                document.body.appendChild(script);
+
+            } 
+            else {
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } 
+        else {
+            limpa_formulário_cep();
+        }
+    };
